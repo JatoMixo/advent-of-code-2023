fc0fa612c5ea4272229897efddb0fec1e138bc39
JatoMixo <jatomixo@outlook.es>
2023-12-07 17:24:29 +0100

    Started day 2

---
+++ day-02/src/lib.rs
@@ -0,0 +1,84 @@
+#[derive(Debug, PartialEq)]
+pub struct Cubes {
+    pub red: u32,
+    pub blue: u32,
+    pub green: u32,
+}
+
+impl Cubes {
+
+    pub fn new() -> Cubes {
+        Cubes {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    pub fn set_color_by_string(&mut self, color: &str, value: u32) {
+        match color {
+            "red" => self.red = value,
+            "green" => self.green = value,
+            "blue" => self.blue = value,
+            _ => println!("You fucked up the color, man"),
+        };
+    }
+
+    pub fn from_string(string: String) -> Cubes {
+        
+        let mut result = Cubes::new();
+
+        let split_string = string.split(", ").collect::<Vec<&str>>();
+
+        split_string.into_iter().for_each(|section| {
+            let color_parsed = section.split(" ").collect::<Vec<&str>>();
+
+            result.set_color_by_string(color_parsed[1], color_parsed[0].parse::<u32>().unwrap());
+        });
+
+        result
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Game {
+    pub id: u32,
+    pub cubes_shown: Vec<Cubes>,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            id: 0,
+            cubes_shown: Vec::new()
+        }
+    }
+
+    pub fn from_string(string: String) -> Game {
+
+        let mut result = Game::new();
+
+        let main_split = string.split(": ").collect::<Vec<&str>>();
+
+        result.id = Game::get_game_id_from_string(main_split[0]);
+        result.cubes_shown = Game::get_cubes_shown_from_string(main_split[1]);
+
+        result
+    }
+
+    fn get_game_id_from_string(string: &str) -> u32 {
+        string.split(" ").collect::<Vec<&str>>()[1].parse::<u32>().unwrap()
+    }
+
+    fn get_cubes_shown_from_string(string: &str) -> Vec<Cubes> {
+
+        let mut result = Vec::new();
+
+        let sections = string.split("; ").collect::<Vec<&str>>();
+        sections.into_iter().for_each(|cube_section| {
+            result.push(Cubes::from_string(cube_section.to_string()));
+        });
+
+        result
+    }
+}
---
+++ day-02/tests/basic_tests.rs
@@ -0,0 +1,24 @@
+use day_02::{Cubes, Game};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cubes_parsing() {
+        assert_eq!(Cubes::from_string("3 blue, 4 red".to_string()), Cubes { blue: 3, red: 4, green: 0 });
+        assert_eq!(Cubes::from_string("1 red, 2 green, 6 blue".to_string()), Cubes { blue: 6, red: 1, green: 2 });
+
+        assert_eq!(Cubes::from_string("1 blue".to_string()), Cubes { blue: 1, red: 0, green: 0 });
+        assert_eq!(Cubes::from_string("6 red, 1 blue, 3 green".to_string()), Cubes { red: 6, blue: 1, green: 3 });
+    }
+
+    #[test]
+    fn test_game_parsing() {
+        assert_eq!(Game::from_string("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()),
+            Game { id: 1, cubes_shown: vec![Cubes {blue: 3, red: 4, green: 0}, Cubes {red: 1, green: 2, blue: 6}, Cubes {green: 2, blue: 0, red: 0 }] });
+        
+        assert_eq!(Game::from_string("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red".to_string()),
+            Game { id: 4, cubes_shown: vec![Cubes {green: 1, red: 3, blue: 6}, Cubes {red: 6, green: 3, blue: 0}, Cubes {green: 3, blue: 15, red: 14}] });
+    }
+}
