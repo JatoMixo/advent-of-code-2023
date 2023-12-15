e0c981d21930b885fb4774cb002ee14e2a0639d0
JatoMixo <jatomixo@outlook.es>
2023-12-15 20:26:07 +0100

    Completed Day 3 Part 1

--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -1,5 +1,57 @@
+fn is_valid_symbol(character: char) -> bool {
+    !character.is_alphanumeric() && character != '.' && character.to_string().trim() == character.to_string()
+}
+
 pub fn calculate_schematic(schematic: String) -> u32 {
     let mut valid_numbers: Vec<u32> = Vec::new();
+    let schematic_lines = schematic.split("\n").collect::<Vec<&str>>();
 
-    valid_numbers.into_iter().sum()
+    for line_index in 0..schematic_lines.len() {
+        
+        let line = schematic_lines[line_index];
+        let mut has_symbol = false;
+        let mut actual_number = 0;
+
+        for character_index in 0..line.len() {
+
+            let character = line.chars().nth(character_index).unwrap();
+
+            match character.to_string().parse::<u32>() {
+                Err(_) => {
+                    if has_symbol {
+                        valid_numbers.push(actual_number);
+                    }
+
+                    actual_number = 0;
+                    has_symbol = false;
+                },
+                Ok(value) => {
+                    actual_number = (actual_number * 10) + value;
+
+                    // Check lateral
+                    let left_index_char = std::cmp::max(character_index as i32 - 1, 0);
+                    let left_character = line.chars().nth(left_index_char as usize).unwrap_or('.');
+                    let right_character = line.chars().nth(character_index + 1).unwrap_or('.');
+
+                    has_symbol |= is_valid_symbol(left_character) || is_valid_symbol(right_character);
+
+                    // Check up and diagonal
+                    let left_index_line = std::cmp::max(line_index as i32 - 1, 0);
+                    let up_line = *schematic_lines.get(left_index_line as usize).unwrap_or(&"...");
+                    let down_line = *schematic_lines.get(line_index + 1).unwrap_or(&"...");
+
+                    for check in 0..3 {
+                        let char_index = std::cmp::max(check as i32 - 1 + character_index as i32, 0);
+                        let char_up = up_line.chars().nth(char_index as usize).unwrap_or('.');
+                        let char_down = down_line.chars().nth(char_index as usize).unwrap_or('.');
+
+                        has_symbol |= is_valid_symbol(char_up) || is_valid_symbol(char_down);
+                    }
+                },
+            }
+        };
+    };
+
+    println!("{}", valid_numbers[2]);
+    valid_numbers.iter().sum()
 }
--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,7 @@
+use day_03::calculate_schematic;
+
 fn main() {
-    println!("It's going...");
+    let input = std::fs::read_to_string("engine_schematic.txt").expect("Forgot the file...");
+
+    println!("{}", calculate_schematic(input));
 }
