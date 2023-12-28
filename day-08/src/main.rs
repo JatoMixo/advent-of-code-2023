9d2cebf3a1165e8065cdbca2da237a6fad7bd78a
JatoMixo <jatomixo@outlook.es>
2023-12-28 20:23:33 +0100

    Finished Day 8 Part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use num_integer::lcm;
 
 #[derive(Debug)]
 struct Map {
@@ -63,8 +64,64 @@ fn calculate_steps(input: String) -> u64 {
     steps
 }
 
+fn calculate_simultaneous_steps(input: String) -> u64 {
+    let (instructions, maps) = input.split_once("\r\n\r\n").unwrap();
+
+    let instructions = instructions.to_string();
+    let maps = get_maps(maps.to_string());
+
+    let mut locations = maps.keys().filter(|&map| map.ends_with("A")).collect::<Vec<&String>>();
+    let mut steps_vec: Vec<u64> = Vec::new();
+    let mut steps = 0;
+
+    loop {
+        let actual_instruction = instructions.chars().nth(steps as usize % instructions.len()).unwrap();
+
+        if locations.iter().filter(|&&map| map.ends_with("Z")).collect::<Vec<&&String>>().len() == locations.len() {
+            break;
+        }
+        
+        steps += 1;
+        for location_index in 0..locations.len() {
+
+            let location = locations[location_index];
+
+            if location.ends_with("Z") {
+                continue;
+            }
+
+            match actual_instruction {
+                'L' => {
+                    let new_location = &maps.get(location).unwrap();
+                    locations[location_index] = &new_location.left;
+                },
+                'R' => {
+                    let new_location = &maps.get(location).unwrap();
+                    locations[location_index] = &new_location.right;
+                },
+                _ => panic!("Wait, what just happened?!"),
+            }
+
+            if locations[location_index].ends_with("Z") {
+                steps_vec.push(steps);
+            }
+        };
+    }
+
+    multiple_lcm(steps_vec)
+}
+
+fn multiple_lcm(vec: Vec<u64>) -> u64 {
+    let mut result = lcm(vec[0], vec[1]);
+    for element_index in 2..vec.len() {
+        result = lcm(result, vec[element_index]);
+    }
+    result
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("File");
 
-    println!("{}", calculate_steps(input));
+    println!("Result 1: {}", calculate_steps(input.clone()));
+    println!("Result 2: {}", calculate_simultaneous_steps(input));
 }
